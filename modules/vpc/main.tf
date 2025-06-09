38f3526a79e228e9f7eddd780d82c941c3cfb5d8
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-09 16:17:05 +0530

    Added 2 more VPC's.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,25 +1,84 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+resource "aws_vpc" "Dev_vpc" {
+  cidr_block = var.dev_vpc_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "Main VPC"
+    Name = "Dev VPC"
     Environment = var.environment
   }
 }
 
+resource "aws_vpc" "QA_vpc" {
+  cidr_block = var.qa_vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "QA VPC"
+    Environment = var.qa_environment
+  }
+  
+}
+
+resource "aws_vpc" "prod_vpc" {
+  cidr_block = var.prod_vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Prod VPC"
+    Environment = var.prod_environment
+  }
+  
+}
+
 resource "aws_subnet" "public_subnet_1" {
-  cidr_block = var.pub-1-cidr-block
-  vpc_id = aws_vpc.main.id
-  availability_zone = var.pub-1-az
+  cidr_block = var.dev-pub-1-cidr-block
+  vpc_id = aws_vpc.Dev_vpc.id
+  availability_zone = var.dev-pub-1-az
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  cidr_block = var.pub-2-cidr-block
-  vpc_id = aws_vpc.main.id
-  availability_zone = var.pub-2-az
+  cidr_block = var.dev-pub-2-cidr-block
+  vpc_id = aws_vpc.Dev_vpc.id
+  availability_zone = var.dev-pub-2-az
   map_public_ip_on_launch = true
 }
 
+resource "aws_subnet" "qa-public_subnet_1" {
+  cidr_block = var.qa-pub-1-cidr-block
+  vpc_id = aws_vpc.QA_vpc.id
+  availability_zone = var.qa-pub-1-az
+  map_public_ip_on_launch = true
+  
+}
+
+resource "aws_subnet" "prod_public_subnet_1" {
+  cidr_block = var.prod-pub-1-cidr-block
+  vpc_id = aws_vpc.prod_vpc.id
+  availability_zone = var.prod-pub-1-az
+  map_public_ip_on_launch = true
+  
+}
+
+resource "aws_subnet" "prod_public_subnet_2" {
+  cidr_block = var.prod-pub-2-cidr-block
+  vpc_id = aws_vpc.prod_vpc.id
+  availability_zone = var.prod-pub-2-az
+  map_public_ip_on_launch = true
+  
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  cidr_block = var.prod-pri-1-cidr-block
+  vpc_id = aws_vpc.prod_vpc.id
+  availability_zone = var.prod-pri-1-az
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  cidr_block = var.prod-pri-2-cidr-block
+  vpc_id = aws_vpc.prod_vpc.id
+  availability_zone = var.prod-pri-2-az
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -4,31 +4,119 @@ variable "environment" {
   default     = "dev"  
 }
 
-variable "vpc_cidr" {
+variable "qa_environment" {
+  description = "The environment for the VPC (e.g., dev, staging, prod)"
+  type        = string
+  default     = "qa"
+}
+
+variable "prod_environment" {
+  description = "The environment for the VPC (e.g., dev, staging, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "qa_vpc_cidr" {
+  description = "CIDR block for the QA VPC"
+  type        = string
+  default     = "125.0.0.0/16"
+  
+}
+
+variable "dev_vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "pub-1-cidr-block" {
+variable "prod_vpc_cidr" {
+  description = "CIDR block for the Production VPC"
+  type        = string
+  default     = "198.0.0.0/16"
+  
+}
+variable "dev-pub-1-cidr-block" {
   description = "CIDR block for the first public subnet"
   type = string
   default = "10.0.1.0/24"
 }
 
-variable "pub-1-az" {
+variable "dev-pub-1-az" {
   description = "Availability Zone for the first public subnet"
   type = string
   default = "us-east-1a"
 }
 
-variable "pub-2-cidr-block" {
+variable "dev-pub-2-cidr-block" {
   description = "CIDR block for the second public subnet"
   type = string
   default = "10.0.3.0/24" 
 }
-variable "pub-2-az" {
+variable "dev-pub-2-az" {
   description = "Availability Zone for the second public subnet"
   type = string
   default = "us-east-1c"
+}
+
+variable "qa-pub-1-cidr-block" {
+  description = "CIDR block for the first public subnet in QA"
+  type = string
+  default = "125.0.1.0/24"
+}
+
+variable "qa-pub-1-az" {
+  description = "Availability Zone for the first public subnet in QA"
+  type = string
+  default = "us-east-1a"
+}
+
+variable "prod-pub-1-cidr-block" {
+  description = "CIDR block for the first public subnet in Production"
+  type = string
+  default = "198.0.1.0/24"
+
+}
+
+variable "prod-pub-1-az" {
+  description = "Availability Zone for the first public subnet in Production"
+  type = string
+  default = "us-west-2a"
+}
+
+variable "prod-pub-2-cidr-block" {
+  description = "CIDR block for the second public subnet in Production"
+  type = string
+  default = "198.0.3.0/24"
+}
+
+variable "prod-pub-2-az" {
+  description = "Availability Zone for the second public subnet in Production"
+  type = string
+  default = "us-west-2b"
+}
+
+variable "prod-pri-1-cidr-block" {
+  description = "CIDR block for the first private subnet in Production"
+  type = string
+  default = "198.0.2.0/24"
+
+}
+
+variable "prod-pri-1-az" {
+  description = "Availability Zone for the first private subnet in Production"
+  type = string
+  default = "us-west-2a"
+
+}
+
+variable "prod-pri-2-cidr-block" {
+  description = "CIDR block for the second private subnet in Production"
+  type = string
+  default = "198.0.2.0/24"
+}
+
+variable "prod-pri-2-az" {
+  description = "Availability Zone for the second private subnet in Production"
+  type = string
+  default = "us-west-2b"
 }
