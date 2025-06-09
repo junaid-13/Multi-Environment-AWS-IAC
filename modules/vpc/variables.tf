dc83e9300e778f7af8d5d822f1d3ac160de626c5
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-09 14:42:51 +0530

    Fix the CIDR errors.

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,34 @@
 variable "environment" {
   description = "The environment for the VPC (e.g., dev, staging, prod)"
   type        = string
-  default     = "dev"
-  
+  default     = "dev"  
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "pub-1-cidr-block" {
+  description = "CIDR block for the first public subnet"
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "pub-1-az" {
+  description = "Availability Zone for the first public subnet"
+  type = string
+  default = "us-east-1a"
+}
+
+variable "pub-2-cidr-block" {
+  description = "CIDR block for the second public subnet"
+  type = string
+  default = "10.0.3.0/24" 
+}
+variable "pub-2-az" {
+  description = "Availability Zone for the second public subnet"
+  type = string
+  default = "us-east-1c"
 }
