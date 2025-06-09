8fe296c4decf16cf0df3106a56e2e287d409fcf4
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-09 23:07:48 +0530

    Trying to fix the provider error with method-1.

--- provider.tf
+++ provider.tf
@@ -8,11 +8,11 @@ terraform {
     required_version = ">= 1.0"
 }
 provider "aws" {
-  alias  = "us-east"
+  alias  = "us-east-1"
   region = "us-east-1"
 }
 
 provider "aws" {
-  alias  = "us-west"
+  alias  = "us-west-2"
   region = "us-west-2"
 }
