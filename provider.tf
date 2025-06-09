856c3e781974ad7fca06870418ed875fc228f8f3
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-09 16:33:08 +0530

    Added multiple regions.

--- provider.tf
+++ provider.tf
@@ -7,3 +7,13 @@ terraform {
   }
     required_version = ">= 1.0"
 }
+
+provider "aws" {
+  alias  = "us_east"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us_west"
+  region = "us-west-2"
+}
