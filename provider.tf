0f9238c2baf7c2c623e5b413e1ddeacc207bc7dd
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-09 21:47:40 +0530

    Added the regions block to test whether it's working properly.

--- provider.tf
+++ provider.tf
@@ -7,3 +7,12 @@ terraform {
   }
     required_version = ">= 1.0"
 }
+provider "aws" {
+  alias  = "us-east"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-west"
+  region = "us-west-2"
+}
