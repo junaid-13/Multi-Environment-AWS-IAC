3509e693f313a92a5750f15b953e0fea5612b8c6
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-12 19:25:13 +0530

    Added the vpc as dev_vpc_id = module.vpc.dev_vpc_id in the main file.

--- main.tf
+++ main.tf
@@ -4,4 +4,7 @@ module "vpc_dev" {
 
 module "security_group" {
   source = "./modules/SG's"
+  dev_vpc_id   = module.vpc.dev_vpc_id
+  qa_vpc_id    = module.vpc.qa_vpc_id
+  prod_vpc_id  = module.vpc.prod_vpc_id
 }
