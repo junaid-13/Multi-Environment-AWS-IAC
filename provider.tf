219ac53a648ed5849f8eb4c856ff2edfe9ac473b
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-16 12:35:04 +0530

    Added the remote configuration

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 terraform {
+backend "s3" {
+  bucket = "devopswale-tfstate"
+  key = "/env/dev/terraform.tfstate"
+  region = "us-east-1"
+  encrypt = true
+  dynamodb_table = "terraform-locks"
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
